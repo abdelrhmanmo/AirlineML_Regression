8281a1fd15855c5fec81041262e764580dcc2e95
Abdelrhman Mohamed <61846392+abdelrhmanmo@users.noreply.github.com>
2022-04-16 11:21:26 +0200

    Update model_test.py

--- model_test.py
+++ model_test.py
@@ -14,7 +14,6 @@ df["date"]=df["date"].dt.strftime("%m/%d/%Y")
 df["Year"]=pd.DatetimeIndex(df["date"]).year
 df["Month"]=pd.DatetimeIndex(df["date"]).month
 df["Day"]=pd.DatetimeIndex(df["date"]).day
-
 lbl_enc = LabelEncoder()
 df['ch_code'] = lbl_enc.fit_transform(df[["ch_code"]])
 
@@ -24,7 +23,17 @@ df['dep_time'] = df['dep_time'].dt.strftime("%-H:%M")
 df["dep_hour"]=pd.DatetimeIndex(df["dep_time"]).hour
 df["dep_minute"]=pd.DatetimeIndex(df["dep_time"]).minute
 
-df=df.drop(["date","airline","dep_time"],axis=1) # drop initial columns
+
+df["hours_taken"] = df["time_taken"].str[:2]
+df["minutes_taken"] = df["time_taken"].str[4:6]
+
+# Stop Column : (Problem : '+2')
+df["stop"] = df["stop"].str.split('-').str.get(0)
+df["stop"] = df["stop"].replace(['non'], 0)
+print(df["stop"])
+
+#df=df.drop(["date","airline","dep_time"],axis=1) # drop initial columns
+df=df.drop(["airline","dep_time","time_taken"],axis=1)
 X = df.loc[:, df.columns != 'price']
 Y = df['price']
 print(X)
